09b96ab45e0fcf8f696041b9c29d7e52d883553e
anggeralmasih <angger.almasih@gmail.com>
2021-12-23 12:22:15 +0700

    Login & Register

---
+++ Rantang.Ku/app/src/main/java/com/angger_19104073/rantangku/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.angger_19104073.rantangku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
